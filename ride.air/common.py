2271a221db6a6fb6422b013367443fccd644159d
lishiyu <shiyu.li@wind.co>
2020-01-21 18:02:45 +0800

    upload ride.air

---
+++ ride.air/common.py
@@ -0,0 +1,94 @@
+# -*- encoding=utf8 -*-
+__author__ = "shiyu.li"
+
+from airtest.core.api import *
+from airtest.core.android.adb import *
+from airtest.core.android.android import *
+from poco.proxy import *
+from poco.drivers.android.uiautomation import AndroidUiautomationPoco
+
+poco = AndroidUiautomationPoco()
+adb = ADB()
+android = Android()
+
+def get_deviceList():
+
+    deviceList = adb.devices()
+    # print(device_list)
+    return deviceList
+
+def check_app(package):
+    
+    # adb shell dumpsys package com.zen.zbike
+    output = adb.shell(['dumpsys', 'package', package])
+    pattern = r'Package\s+\[' + str(package) + '\]'
+    match = re.search(pattern, output)
+    if match is None:
+        return False
+        # raise AirtestError('package "{}" not found'.format(package))
+    return True
+
+def start(package, install_path):
+    
+    try:
+        if android.check_app(package):
+            clear_app(package)
+        else:
+            try:
+                install(install_path)
+            except:
+                print('FAILED')
+    except:
+        install(install_path)
+                
+    clear_app(package)
+    start_app(package)
+
+def stop(package):
+    
+    clear_app(package)
+    stop_app(package)
+    
+def exists(value):
+    try:
+        return element.attr('visible')
+    except:
+        return False
+    
+def assert_exists(img, msg='test'):
+
+    try:
+        pos = loop_find(Template(img), timeout=ST.FIND_TIMEOUT, threshold=ST.THRESHOLD_STRICT)
+        return pos
+    except TargetNotFoundError:
+        raise AssertionError("%s does not exist in screen, message: %s" % (img, msg))
+
+def UItouch(img):
+
+    assert_exists(img)
+    touch(Template(img, record_pos=(0.292, 0.027), resolution=(1080, 2220)))
+
+def click(element, timeout = 60):
+    
+    poco(element).wait_for_appearance(timeout)
+    try:
+        poco(element).click()
+    except():
+        print('An exception occured!')
+
+# 设置UI元素的属性        
+def set_text(element, content):
+    
+    try:
+        poco(element).set_text(content)
+    except:
+        print('An exception occured!')
+        
+# adb shell的命令去发送广播，然后有一个listener要接收这个广播
+def sendKeys(content):
+    
+    try:
+        text(content)
+    except:
+        print('An exception occured!')
+        
---
+++ ride.air/ride.py
@@ -0,0 +1,203 @@
+# -*- encoding=utf8 -*-
+__author__ = "shiyu.li"
+
+from common import *
+
+scooterNo="S9111117"
+
+arr = [
+    {
+        "type":"click",
+        "element":"scan_to_unlock",
+        "assert":{
+            "img":"tpl1579585539562.png"  
+                 },
+        "click":{
+            "value":"com.zen.zbike:id/button_scan_to_unlock"
+        }
+    },
+    {
+        "type":"permisson_allow",
+        "element":"permisson_camera",
+        "assert":{
+            "allow": "tpl1579585793615.png"
+        },
+         "click":{
+           "value":"com.android.packageinstaller:id/permission_allow_button",
+        }
+    },
+    {
+        "type":"click",
+        "element":"input_scooterNo_button",
+        "assert":{
+            "img":"tpl1579585946910.png"
+        },
+        "click":{
+            "value":"com.zen.zbike:id/button_input_number"
+        }
+    },
+    {
+        "type":"click",
+        "element":"scooterNo_inputbox",
+         "assert":{
+            "img": "tpl1579587002625.png"
+        },
+        "click":{
+            "value":"com.zen.zbike:id/bike_number_edit_text"
+        }
+    },
+    {
+        "type":"sendText",
+        "element":"scooterNo",
+        "assert":{
+            "img":"tpl1579587273324.png"
+        },
+        "sendText":{
+            "value":"com.zen.zbike:id/bike_number_edit_text",
+            "text":scooterNo
+        }
+    },
+    {
+        "type":"click",
+        "element":"unlock",
+        "assert":{
+            "img":"tpl1579587318998.png"
+        },
+        "click":{
+            "value":"com.zen.zbike:id/button_unlock"
+        }
+    },
+    {
+        "type":"notice",
+        "element":"unlock_failed",
+        "assert":{
+            "unlock_failed":"tpl1579592455098.png"
+        },
+        "click":
+        {
+            "value":"android.widget.LinearLayout"
+        }
+    },
+    {
+        "type":"click",
+        "element":"how_to_ride",
+        "assert":{
+            "img":"tpl1579587590905.png"
+        },
+        "click":{
+            "value":"com.zen.zbike:id/got_view"
+        }
+    },
+    {
+        "type":"sleep",
+        "time":90
+    },
+    {
+        "type":"click",
+        "element":"lock",
+        "assert":{
+            "duration":"tpl1579587867601.png",
+            "cost":"tpl1579587874787.png"
+        },
+        "click":{
+            "value":"com.zen.zbike:id/end_to_ride_btn"
+        }
+    },
+    {
+        "type":"click",
+        "element":"park_got_it",
+        "assert":{
+            "img":"tpl1579588771290.png"
+        },
+        "click":{
+            "value":"com.zen.zbike:id/got_view"
+        }
+    },
+    {
+        "type":"sleep",
+        "time":15
+    },
+    {
+        "type":"permisson_allow",
+        "element":"permisson_phtots",
+        "assert":{
+            "folder": "tpl1579588909789.png"
+        },
+         "click":{
+           "value":"com.android.packageinstaller:id/permission_allow_button",
+        }
+    },
+    {
+        "type":"click",
+        "element":"take_photo",
+        "assert":{
+            "img":"tpl1579593043881.png"
+        },
+        "click":{            
+          "value":"com.zen.zbike:id/button_back"
+        }
+    },
+    {
+        "type":"rate_us",
+        "element":"rate_us",
+        "assert":{
+            "img":"tpl1579589165493.png"
+        },
+        "click":{
+            "value":"com.zen.zbike:id/cancel_rating"
+        }
+    },
+    {
+        "type":"questionnaire",
+        "element":"question_answer",
+        "assert":{
+            "img":"tpl1579589666843.png"
+        },
+        "click":{
+            "value":"com.zen.zbike:id/checkbox_view"
+        }
+    },
+    {
+        "type":"clcik",
+        "element":"questionnaire_submit",
+        "assert":{
+            "img":"tpl1579589686172.png"
+        },
+        "click":{
+            "value":"com.zen.zbike:id/submit_view"
+        }
+    },
+    {
+        "type":"click",
+        "element":"trip_sumamry",
+        "assert":{
+            "trip_summary":"tpl1579589712018.png",
+            "trip_cost":"tpl1579589733523.png",
+            "reload":"tpl1579589756688.png"
+        },
+         "click":{
+            "value":"com.zen.zbike:id/back_view"
+        }
+    }
+]
+
+def run(arr,scooterNo):
+        
+    for i in arr:
+        if i["type"] is "click":
+            for img in i["assert"]:
+                assert_exists(i["assert"][img])
+            click(i["click"]["value"])
+        elif i["type"] in ("permisson_allow", "notice", "rate_us",
+                           "questionnaire"):
+            if exists(i["click"]["value"]) is True:
+                click(i["click"]["value"])
+            else:
+                continue
+        elif i["type"] is "sendText":
+            assert_exists(i["assert"]["img"])
+            set_text(i["sendText"]["value"], i["sendText"]["text"])
+        elif i["type"] is "sleep":
+            sleep(i["time"])                
+
+run(arr,scooterNo)
