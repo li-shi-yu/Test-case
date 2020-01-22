4abb0903e128cde8ffeead79861493fa1470e0f7
lishiyu <shiyu.li@wind.co>
2020-01-22 15:21:19 +0800

    update test

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
 # -*- encoding=utf8 -*-
 
 print('hello')
+
+opts, args = getopt.getopt(sys.argv[1:], "s:",
+["help", "src=", "dst=", "major=", "version=", "platform=", "channels="])
+
+for option in opts:
+	print(option)
