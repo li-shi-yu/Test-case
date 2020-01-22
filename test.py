c774c44719d9862cc0915603129fa6693909ddc6
lishiyu <shiyu.li@wind.co>
2020-01-22 16:34:26 +0800

    test.py updated

--- test.py
+++ test.py
@@ -3,5 +3,9 @@
 # print('hello')
 
 import os
+testcasename = os.getenv("testcasename")
+devices = os.getenv("device")
 scooterNo = os.getenv("scooterNo")
+print(scooterNo)
+print(devices)
 print(scooterNo)
