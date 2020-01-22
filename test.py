2f83a54836fa1bcf010b0c3b80c80a5f1074f949
lishiyu <shiyu.li@wind.co>
2020-01-22 15:39:24 +0800

    test

--- test.py
+++ test.py
@@ -1,11 +1,7 @@
 # -*- encoding=utf8 -*-
 
-print('hello')
+# print('hello')
 
-import sys,getopt
-
-opts, args = getopt.getopt(sys.argv[1:], "s:",
-["help", "src=", "dst=", "major=", "version=", "platform=", "channels="])
-
-for option in opts:
-	print(option)
+import os
+scooterNo = os.getenv("scooterNo")
+print(scooterNo)
